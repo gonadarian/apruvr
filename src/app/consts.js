83bc598e587c646d1da313812ed1ba71eac0521e
Igor Popov <gonadarian@gmail.com>
2016-10-06 23:27:46 +0200

    Reformated constants objects.

--- src/app/consts.js
+++ src/app/consts.js
@@ -1,77 +1,24 @@
 export const CONTENTS = [
-    {
-        code: 'exercises',
-        name: 'Exercises',
-    },
-    {
-        code: 'articles',
-        name: 'Articles',
-    },
-    {
-        code: 'videos',
-        name: 'Videos',
-    },
-    {
-        code: 'scratchpads',
-        name: 'Scratchpads',
-    },
+    { code: 'exercises', name: 'Exercises' },
+    { code: 'articles', name: 'Articles' },
+    { code: 'videos', name: 'Videos' },
+    { code: 'scratchpads', name: 'Scratchpads' },
 ];
 
 export const LANGUAGES = [
-    {
-        code: 'bg',
-        name: 'Bulgarian',
-    },
-    {
-        code: 'nl',
-        name: 'Dutch',
-    },
-    {
-        code: 'fr',
-        name: 'French',
-    },
-    {
-        code: 'de',
-        name: 'German',
-    },
-    {
-        code: 'gr',
-        name: 'Greek',
-    },
-    {
-        code: 'it',
-        name: 'Italian',
-    },
-    {
-        code: 'ko',
-        name: 'Korean',
-    },
-    {
-        code: 'pl',
-        name: 'Polish',
-    },
-    {
-        code: 'pt-pt',
-        name: 'Portuguese',
-        note: 'Portugal',
-    },
-    {
-        code: 'ru',
-        name: 'Russian',
-    },
-    {
-        code: 'sr',
-        name: 'Serbian',
-        note: 'Cyrillic',
-    },
-    {
-        code: 'es',
-        name: 'Spanish',
-    },
-    {
-        code: 'tr',
-        name: 'Turkish',
-    },
+    { code: 'bg', name: 'Bulgarian' },
+    { code: 'nl', name: 'Dutch' },
+    { code: 'fr', name: 'French' },
+    { code: 'de', name: 'German' },
+    { code: 'gr', name: 'Greek' },
+    { code: 'it', name: 'Italian' },
+    { code: 'ko', name: 'Korean' },
+    { code: 'pl', name: 'Polish' },
+    { code: 'pt-pt', name: 'Portuguese', note: 'Portugal' },
+    { code: 'ru', name: 'Russian' },
+    { code: 'sr', name: 'Serbian', note: 'Cyrillic' },
+    { code: 'es', name: 'Spanish' },
+    { code: 'tr', name: 'Turkish' },
 ];
 
 export const VISIBILITIES = {
