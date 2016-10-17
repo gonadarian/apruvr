da9a93a490aa3d876ed925cb8c1abf0de849f34d
Igor Popov <gonadarian@gmail.com>
2016-10-17 23:21:49 +0200

    Removed unneeded brackets.

--- src/app/reducers/workflowReducer.js
+++ src/app/reducers/workflowReducer.js
@@ -2,9 +2,7 @@ import { FIREBASE_FETCH_ONCE } from '../actions/types';
 import handleReducers from './handler';
 
 const handlers = {
-    [FIREBASE_FETCH_ONCE]: (state, action) => {
-        return action.payload.val();
-    },
+    [FIREBASE_FETCH_ONCE]: (state, action) => action.payload.val(),
 };
 
 export default (state = null, action) =>
