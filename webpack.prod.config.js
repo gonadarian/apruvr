fb0d926dd729565953da10a312adcafb0a6f08d9
Igor Popov <gonadarian@gmail.com>
2016-10-17 23:42:57 +0200

    Reusing common setup from dev webpack config.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,44 +1,15 @@
-const path = require('path');
 const webpack = require('webpack');
+const devConfig = require('./webpack.config');
 
-const BUILD_DIR = path.resolve(__dirname, 'src/public');
-const APP_DIR = path.resolve(__dirname, 'src/app');
-
-const config = {
-    entry:  APP_DIR + '/index.jsx',
-    output: {
-        path:       BUILD_DIR,
-        filename:   'bundle.min.js',
-    },
-    resolve: {
-        extensions: [
-            '',
-            '.js',
-            '.jsx',
+const config = Object.assign(
+    devConfig,
+    {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: { warnings: false },
+            }),
         ],
-    },
-    module: {
-        loaders: [
-            {
-                test:       /\.jsx?/,
-                loader:     'babel',
-                include:    APP_DIR,
-            },
-            {
-                test:       /\.css$/,
-                loader:     'style!css',
-            },
-            {
-                test:       /\.less$/,
-                loader:     'style!css?modules!less',
-            },
-        ],
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-        }),
-    ],
-};
+    }
+);
 
 module.exports = config;
