aff94335c17f5e4ad73adbcda9c99e9f0be2006e
Igor Popov <gonadarian@gmail.com>
2016-10-27 19:19:07 +0200

    Using arrows instead of old function definitions.

--- src/app/hocs/fireduxed.jsx
+++ src/app/hocs/fireduxed.jsx
@@ -1,29 +1,28 @@
 import React, { PropTypes, Component } from 'react';
 
-export default function fireduxed(calcPath) {
-    return (WrappedComponent) => {
-        return class FireduxedHOC extends Component {
-            static contextTypes = {
-                firebase: PropTypes.object,
-            }
+const fireduxed = (calcPath) => (WrappedComponent) =>
+    class FireduxedHOC extends Component {
+        static contextTypes = {
+            firebase: PropTypes.object,
+        }
 
-            static propTypes = {
-                onFire: PropTypes.func.isRequired,
-            }
+        static propTypes = {
+            onFire: PropTypes.func.isRequired,
+        }
 
-            componentDidMount() {
-                const path = calcPath(this.props);
-                this.ref = this.context.firebase.database().ref(path);
-                this.ref.on('value', (snapshot) => this.props.onFire(snapshot));
-            }
+        componentDidMount() {
+            const path = calcPath(this.props);
+            this.ref = this.context.firebase.database().ref(path);
+            this.ref.on('value', (snapshot) => this.props.onFire(snapshot));
+        }
 
-            componentWillUnmount() {
-                this.ref.off();
-            }
+        componentWillUnmount() {
+            this.ref.off();
+        }
 
-            render() {
-                return <WrappedComponent {...this.props} />;
-            }
-        };
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
     };
-}
+
+export default fireduxed;
