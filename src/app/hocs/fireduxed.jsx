64926685e32a41c43c7f0e11259f82c38b263226
Igor Popov <gonadarian@gmail.com>
2016-11-13 13:39:33 +0100

    One more move to global Firebase app usage.

--- src/app/hocs/fireduxed.jsx
+++ src/app/hocs/fireduxed.jsx
@@ -1,18 +1,15 @@
 import React, { PropTypes, Component } from 'react';
+import firebase from 'firebase';
 
 const fireduxed = (calcPath) => (WrappedComponent) =>
     class FireduxedHOC extends Component {
-        static contextTypes = {
-            firebase: PropTypes.object,
-        }
-
         static propTypes = {
             onFire: PropTypes.func.isRequired,
         }
 
         componentDidMount() {
             const path = calcPath(this.props);
-            this.ref = this.context.firebase.database().ref(path);
+            this.ref = firebase.database().ref(path);
             this.ref.on(
                 'value',
                 (snapshot) => this.props.onFire(snapshot)
