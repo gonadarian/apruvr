5b5531c5216ea5a7df97f57163ab3446af9b6f79
Igor Popov <gonadarian@gmail.com>
2016-11-22 20:38:03 +0100

    Further configuring Flow.

--- src/app/flows.js
+++ src/app/flows.js
@@ -2,5 +2,10 @@ export type UserType = {
     uid: string,
     displayName: string,
     email: string,
-    photoURL: string
+    photoURL: string,
+};
+
+export type ItemType = {
+    code: string,
+    name: string,
 };
