122e9f1be46c86787fb91edcd13d210d21457911
Igor Popov <gonadarian@gmail.com>
2016-12-04 22:34:07 +0100

    Removed unused containers.

--- src/app/containers/index.js
+++ src/app/containers/index.js
@@ -1,11 +1,9 @@
 /* @flow */
 import AgentPicker from './AgentPicker';
 import Content from './Content';
-import ContentKindButtons from './ContentKindButtons';
 import ContentKindPicker from './ContentKindPicker';
 import ExporterButton from './ExporterButton';
 import FilteredContentList from './FilteredContentList';
-import LanguageButtons from './LanguageButtons';
 import LanguagePicker from './LanguagePicker';
 import LoadingSpinner from './LoadingSpinner';
 import SelectedTopicList from './SelectedTopicList';
@@ -16,11 +14,9 @@ import VisibilityButtons from './VisibilityButtons';
 export {
     AgentPicker,
     Content,
-    ContentKindButtons,
     ContentKindPicker,
     ExporterButton,
     FilteredContentList,
-    LanguageButtons,
     LanguagePicker,
     LoadingSpinner,
     SelectedTopicList,
