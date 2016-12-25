3ce35bd6df26b77ac66ac370b026eb3c0e02875b
Igor Popov <gonadarian@gmail.com>
2016-12-26 00:59:04 +0100

    Added stats at the end of lists.

--- src/app/components/CrowdinList.jsx
+++ src/app/components/CrowdinList.jsx
@@ -1,10 +1,60 @@
 /* @flow */
 import React from 'react';
 import type { Element } from 'react';
-import { map } from 'lodash';
+import { map, transform } from 'lodash';
 import { CONTENT_LETTERS } from '../consts';
 import type { CrowdinNodeType, NodeMapType, ItemType } from '../flows';
 import CrowdinItem from './CrowdinItem';
+
+type StatType = { cnt: number, sum: number };
+type StatsType = { totl: StatType, trns: StatType, appr: StatType };
+
+const calcStats = (nodes: NodeMapType): StatsType =>
+    transform(
+        nodes,
+        (mem: StatsType, node: CrowdinNodeType) => {
+            mem.totl.cnt += 1;
+            mem.totl.sum += node.wordCount;
+            mem.trns.cnt += node.translatedWordCount === node.wordCount ? 1 : 0;
+            mem.trns.sum += node.translatedWordCount;
+            mem.appr.cnt += node.approvedWordCount === node.wordCount ? 1 : 0;
+            mem.appr.sum += node.approvedWordCount;
+        },
+        {
+            totl: { cnt: 0, sum: 0 },
+            trns: { cnt: 0, sum: 0 },
+            appr: { cnt: 0, sum: 0 },
+        }
+    );
+
+type PropsStatsType = {
+    stats: StatsType,
+};
+
+const CrowdinStats = ({ stats: { totl, trns, appr } }: PropsStatsType): ?Element<*> =>
+    totl.cnt === 0 ? null :
+        <tr className="active">
+            <th />
+            <th>Total</th>
+            <th>
+                words:<br />
+                items:
+            </th>
+            <th>
+                {`${trns.sum} / ${totl.sum} `}
+                <span className="badge">{`${Math.floor(100 * trns.sum / totl.sum)}%`}</span>
+                <br />
+                {`${trns.cnt} / ${totl.cnt} `}
+                <span className="badge">{`${Math.floor(100 * trns.cnt / totl.cnt)}%`}</span>
+            </th>
+            <th>
+                {`${appr.sum} / ${totl.sum} `}
+                <span className="badge">{`${Math.floor(100 * appr.sum / totl.sum)}%`}</span>
+                <br />
+                {`${appr.cnt} / ${totl.cnt} `}
+                <span className="badge">{`${Math.floor(100 * appr.cnt / totl.cnt)}%`}</span>
+            </th>
+        </tr>;
 
 type PropsType = {
     type: string,
@@ -16,7 +66,7 @@ const CrowdinList = ({ type, nodes, ...other }: PropsType): Element<*> =>
     <div>
         <table className="table">
             <thead>
-                <tr>
+                <tr className="active">
                     <th>Name</th>
                     <th>Agent</th>
                     <th>Status</th>
@@ -35,6 +85,9 @@ const CrowdinList = ({ type, nodes, ...other }: PropsType): Element<*> =>
                             content={node} />
                 )}
             </tbody>
+            <tfoot>
+                <CrowdinStats stats={calcStats(nodes)} />
+            </tfoot>
         </table>
     </div>;
 
--- src/app/components/VideoList.jsx
+++ src/app/components/VideoList.jsx
@@ -1,9 +1,42 @@
 /* @flow */
 import React from 'react';
 import type { Element } from 'react';
-import { map } from 'lodash';
+import { map, transform } from 'lodash';
 import type { VideoNodeType, NodeMapType, ItemType } from '../flows';
 import VideoItem from './VideoItem';
+
+type StatsType = { totl: number, subd: number, dubd: number };
+
+const calcStats = (nodes: NodeMapType): StatsType =>
+    transform(
+        nodes,
+        (mem: StatsType, node: VideoNodeType) => {
+            mem.totl += 1;
+            mem.subd += node.subbed ? 1 : 0;
+            mem.dubd += node.dubbed ? 1 : 0;
+        },
+        { totl: 0, subd: 0, dubd: 0 }
+    );
+
+type PropsStatsType = {
+    stats: StatsType,
+};
+
+const VideoStats = ({ stats: { totl, subd, dubd } }: PropsStatsType): ?Element<*> =>
+    totl === 0 ? null :
+        <tr className="active">
+            <th />
+            <th>Total</th>
+            <th>items:</th>
+            <th>
+                {`${subd} / ${totl} `}
+                <span className="badge">{`${Math.floor(100 * subd / totl)}%`}</span>
+            </th>
+            <th>
+                {`${dubd} / ${totl} `}
+                <span className="badge">{`${Math.floor(100 * dubd / totl)}%`}</span>
+            </th>
+        </tr>;
 
 type PropsType = {
     nodes: NodeMapType,
@@ -14,12 +47,12 @@ const VideoList = ({ nodes, ...other }: PropsType): Element<*> =>
     <div>
         <table className="table">
             <thead>
-                <tr>
+                <tr className="active">
                     <th>Name</th>
                     <th>Agent</th>
                     <th>Status</th>
-                    <th>Subtitling</th>
-                    <th>Dubbing</th>
+                    <th>Subtitle</th>
+                    <th>Dub</th>
                 </tr>
             </thead>
             <tbody>
@@ -32,6 +65,9 @@ const VideoList = ({ nodes, ...other }: PropsType): Element<*> =>
                             content={node} />
                 )}
             </tbody>
+            <tfoot>
+                <VideoStats stats={calcStats(nodes)} />
+            </tfoot>;
         </table>
     </div>;
 
