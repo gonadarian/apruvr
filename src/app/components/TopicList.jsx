41599189dfc44e45266a50f83758dfc412e45f4b
Igor Popov <gonadarian@gmail.com>
2016-12-26 01:13:41 +0100

    Added summary row to Topic list, too.

--- src/app/components/TopicList.jsx
+++ src/app/components/TopicList.jsx
@@ -1,10 +1,50 @@
 /* @flow */
 import React from 'react';
 import type { Element } from 'react';
-import { map } from 'lodash';
+import { map, transform } from 'lodash';
 import type { TopicNodeType, NodeMapType, ItemType } from '../flows';
 import { CONTENT_LETTERS } from '../consts';
 import TopicItem from './TopicItem';
+
+type StatType = { cnt: number, sum: number };
+type StatsType = { totl: StatType, trns: StatType };
+
+const calcStats = (nodes: NodeMapType): StatsType =>
+    transform(
+        nodes,
+        (mem: StatsType, node: TopicNodeType) => {
+            mem.totl.cnt += 1;
+            mem.totl.sum += node.metadataWordCount;
+            mem.trns.cnt += node.metadataTranslatedWordCount === node.metadataWordCount ? 1 : 0;
+            mem.trns.sum += node.metadataTranslatedWordCount;
+        },
+        {
+            totl: { cnt: 0, sum: 0 },
+            trns: { cnt: 0, sum: 0 },
+        }
+    );
+
+type PropsStatsType = {
+    stats: StatsType,
+};
+
+const TopicStats = ({ stats: { totl, trns } }: PropsStatsType): ?Element<*> =>
+    totl.cnt === 0 ? null :
+        <tr className="active">
+            <th />
+            <th>Total</th>
+            <th>
+                words:<br />
+                items:
+            </th>
+            <th>
+                {`${trns.sum} / ${totl.sum} `}
+                <span className="badge">{`${Math.floor(100 * trns.sum / totl.sum)}%`}</span>
+                <br />
+                {`${trns.cnt} / ${totl.cnt} `}
+                <span className="badge">{`${Math.floor(100 * trns.cnt / totl.cnt)}%`}</span>
+            </th>
+        </tr>;
 
 type PropsType = {
     type: string,
@@ -34,6 +74,9 @@ const TopicList = ({ type, nodes, ...other }: PropsType): Element<*> =>
                             content={node} />
                 )}
             </tbody>
+            <tfoot>
+                <TopicStats stats={calcStats(nodes)} />
+            </tfoot>
         </table>
     </div>;
 
