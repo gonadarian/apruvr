8d10c5fc78890ac1e4f0cef40cfed16261a386fa
Igor Popov <gonadarian@gmail.com>
2018-05-26 13:18:08 +0200

    More code comments for selectors added.

--- src/app/selectors/filteredNodesSelector.js
+++ src/app/selectors/filteredNodesSelector.js
@@ -4,42 +4,66 @@ import { forIn, reduce, forEach, transform } from 'lodash';
 import { NAMES } from '../consts';
 import topicTreeSelector from './topicTreeSelector';
 
-const loadSlugs = (topic, slugs, path) => {
+/**
+ * Loads full topics in a list and full paths to items of other content types.
+ * @param  {Object}   topic topic tree to recursivelly traverse
+ * @param  {Object}   slugs structure to store found data
+ * @param  {string[]} path  slug array representing full path
+ * @return {Object}         content list with topics and paths for other content types
+ */
+const loadSlugs = (topic, slugs, path: string[]) => {
+    // add current topic to the topic list, recursion will add them all
     slugs.topics[topic.slug] = topic;
-
+    // get and store full paths for all slugs
     forEach(
+        // topic children don't include topics themselves, only true content types
         topic.children,
         (child) => {
-            slugs[NAMES[child[0]]][child[1]] = path;
+            const [contentName, contentSlug] = child;
+            slugs[NAMES[contentName]][contentSlug] = path;
         }
     );
-
+    // go through the topic tree recursively
     forIn(
         topic.topics,
-        (current) => loadSlugs(current, slugs, [...path, current.slug])
+        (current) =>
+            // load for each sub-topic, and pass the path extended by current slug
+            loadSlugs(current, slugs, [...path, current.slug])
     );
 };
 
+/**
+ * Filter the nodes for given topic and content type.
+ * @param  {Object} nodes   flat list of nodes, grouped by content type
+ * @param  {string} topic   path to parent topic, dot separated list of slugs from hierarchy
+ * @param  {string} content chosen content type to filter by
+ * @param  {[type]} tree    pre-generated tree of topics
+ * @return {[type]}         those nodes of given types and descendants of given content type
+ */
 const filterNodes = (nodes, topic, content, tree) => {
+    // remove starting 'root' from the topic path, e.g. from 'root.math' to ['math']
     const startPath = topic.split('.').slice(1);
+    // go through the topic path, slug by slug, and traverse the tree to find target topic
     const localTopic = reduce(
         startPath,
         (currTopic, slug) => currTopic.topics[slug],
         tree.root
     );
-
+    // prepare the resulting slug list...
     const slugs = { exercises: {}, articles: {}, videos: {}, scratchpads: {}, topics: {} };
+    // ... and populate it with paths alone
     loadSlugs(localTopic, slugs, startPath);
-
+    // take only those nodes of the given content type
     const contentNodes = nodes[content.code];
+    // extend node data with information about the topic path to it
     const filtered = transform(
         slugs[content.code],
         (result, path, slug) => {
+            // take the entire node plus add topic path
             result[slug] = { ...contentNodes[slug], path };
         },
         {}
     );
-
     return filtered;
 };
 
@@ -50,5 +74,6 @@ export default createSelector(
         (state) => state.content,
         topicTreeSelector,
     ],
-    (nodes, topic, content, tree) => filterNodes(nodes, topic, content, tree)
+    (nodes, topic, content, tree) =>
+        filterNodes(nodes, topic, content, tree)
 );
--- src/app/selectors/visibleNodesSelector.js
+++ src/app/selectors/visibleNodesSelector.js
@@ -4,7 +4,14 @@ import { pickBy } from 'lodash';
 import { CONTENT_GROUPS } from '../consts';
 import filteredNodesSelector from './filteredNodesSelector';
 
-const isVisibleCrowdin = (node, visibility) => {
+/**
+ * Checks to see whether given Crowdin based node (exercise, article, scratchpad) should be made
+ * visible according to visibility filters.
+ * @param  {Object}  node       content item to check
+ * @param  {Object}  visibility filter to apply
+ * @return {Boolean}            whether node should be visible or not
+ */
+const isVisibleCrowdin = (node, visibility): boolean => {
     const totl = node.translatableWordCount;
     const trns = node.translatedWordCount;
     const appr = node.approvedWordCount;
@@ -17,6 +24,12 @@ const isVisibleCrowdin = (node, visibility) => {
     return isTrns || isAppr || isFrsh || isDoin;
 };
 
+/**
+* Checks to see whether given video node should be made visible according to visibility filters.
+* @param  {Object}  node       content item to check
+* @param  {Object}  visibility filter to apply
+* @return {Boolean}            whether node should be visible or not
+ */
 const isVisibleVideo = (node, visibility) => {
     const isDubd = node.dubbed && visibility.dubbed;
     const isSubd = node.subbed && !node.dubbed && visibility.subtitled;
@@ -25,7 +38,13 @@ const isVisibleVideo = (node, visibility) => {
     return isSubd || isDubd || isFrsh;
 };
 
-const isVisibleTopic = (node, visibility) => {
+/**
+* Checks to see whether given topic node should be made visible according to visibility filters.
+* @param  {Object}  node       content item to check
+* @param  {Object}  visibility filter to apply
+* @return {Boolean}            whether node should be visible or not
+ */
+const isVisibleTopic = (node, visibility): boolean => {
     const totl = node.metadataWordCount;
     const trns = node.metadataTranslatedWordCount;
     const appr = node.metadataApprovedWordCount;
@@ -38,14 +57,25 @@ const isVisibleTopic = (node, visibility) => {
     return isTrns || isAppr || isFrsh || isDoin;
 };
 
-const VISIBILITIES = {
+const VISIBILITY_CHECKERS = {
     crowdin: isVisibleCrowdin,
     videos:  isVisibleVideo,
     topics:  isVisibleTopic,
 };
 
+/**
+* Filter all the nodes according to visibility filters.
+* @param  {Object}  nodes       content items to check
+* @param  {string}  content     content type chosen
+* @param  {Object}  visibility  filter to apply
+* @return {Object}              filtered list of nodes
+ */
 const visibleNodes = (nodes, content, visibility) => {
-    const isVisible = VISIBILITIES[CONTENT_GROUPS[content.code]];
+    // mapping from contenty type to content group
+    const group = CONTENT_GROUPS[content.code];
+    // get viibilit checker for given content group
+    const isVisible = VISIBILITY_CHECKERS[group];
+    // filter the nodes via pickBy for objects which is the same as map is for arrays
     const filtered = pickBy(
         nodes,
         (node) => isVisible(node, visibility)
