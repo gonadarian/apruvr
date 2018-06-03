d4b1b78bb189d8bcc61c6d9e32baf6d6237152bb
Igor Popov <gonadarian@gmail.com>
2018-06-03 14:43:35 +0200

    Replaced Axios data with data on Firebase DB and Prop-Types with Flow.

--- src/app/hocs/firedux.jsx
+++ src/app/hocs/firedux.jsx
@@ -1,6 +1,5 @@
 /* @flow */
 import React, { Component, type ComponentType } from 'react';
-import PropTypes from 'prop-types';
 import firebase from '@firebase/app';
 import '@firebase/database';
 
@@ -8,12 +7,8 @@ type PropsType = {
     onFire: (snapshot: any) => void,
 };
 
-const firedux = <T: PropsType>(calcPath: (any) => string) => (WrappedComponent: ComponentType<T>) =>
-    class HOC extends Component<T> {
-        static propTypes = {
-            onFire: PropTypes.func.isRequired,
-        }
-
+const firedux = (calcPath: (any) => string) => (WrappedComponent: ComponentType<PropsType>) =>
+    class HOC extends Component<PropsType> {
         componentDidMount () {
             const path = calcPath(this.props);
             this.ref = firebase.database().ref(path);
